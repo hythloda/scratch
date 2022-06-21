e4980a9635c00c9bdfbd0269e37afebf8f01638c
Amanda L Martin <hythloda@gmail.com>
2022-06-21 16:59:58 -0400

    numbers

--- sctarch.java
+++ sctarch.java
@@ -1,5 +1,5 @@
 public class Numbers {
-    public static long sum(List<Integers> number){
+    public static long sum(List<Integer> numbers){
         int sum = numbers.stream()
 	                .reduce(0, (a, b) -> a + b);
         return sum;
